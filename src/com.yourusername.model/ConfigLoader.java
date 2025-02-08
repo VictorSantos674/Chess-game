20e2273613e84b8def716631a3b0d4f5e1bb694f
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-02-08 05:33:05 +0000

    :card_file_box: feat: Loader de Configuração

---
+++ src/com.yourusername.model/ConfigLoader.java
@@ -0,0 +1,17 @@
+package com.yourusername.model;
+
+import org.json.JSONObject;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConfigLoader {
+    public static JSONObject carregarConfiguracao() {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("resources/config.json")));
+            return new JSONObject(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
